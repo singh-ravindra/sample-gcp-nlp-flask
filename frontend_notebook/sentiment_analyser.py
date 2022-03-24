7aa19ac31c28a628c067a1aaa407a62678e3ce4b
Ravindra Singh <ravindra.yadav02@gmail.com>
2022-03-24 13:10:44 -0400

    GEE-NLP: add article excel file

--- frontend_notebook/sentiment_analyser.py
+++ frontend_notebook/sentiment_analyser.py
@@ -1,20 +1,9 @@
 # import libraries
 # %matplotlib inline
 
-import requests
-import json
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-from jupyter_dash import JupyterDash
-from dash import dcc
-from dash import html
-from dash.dependencies import Input, Output, State
 from google.cloud import language_v1 as language
-
 
 # Set display row/column to show all data
 pd.set_option('display.max_rows', None)
@@ -72,6 +61,34 @@ def sentiment_analyzer_orchestrator():
 
         print('sentiments', sentiments)
 
+def getsentiment(text):
+    """
+    This is modified from the Google NLP API documentation found here:
+    https://cloud.google.com/natural-language/docs/analyzing-sentiment
+    It makes a call to the Google NLP API to retrieve sentiment analysis.
+    """
+    import os
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
+
+    client = language.LanguageServiceClient()
+    document = language.Document(content=text, type=language.Document.Type.PLAIN_TEXT)
+
+    response = client.analyze_sentiment(document=document)
+
+    # Format the results as a dictionary
+    sentiment = response.document_sentiment
+    results = dict(
+        text=text,
+        score=f"{sentiment.score:.1%}",
+        magnitude=f"{sentiment.magnitude:.1%}",
+    )
+    return sentiment.score
+
 
 if __name__ == "__main__":
-    sentiment_analyzer_orchestrator()
+    articles_df = pd.read_excel('articles/articles.xlsx', parse_dates=[0])
+    articles_df['Sentiment'] = articles_df.apply(lambda row: getsentiment(row['Text']), axis=1)
+    fig = px.scatter(articles_df, x='Date', y='Sentiment', color='Language')
+    fig.savefig('sentiment plot.jpg', bbox_inches='tight', dpi=150)
+    fig.show()
+    # sentiment_analyzer_orchestrator()
